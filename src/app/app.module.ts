b2d7e24e0f05038346b1edb2133dd1b04817c6c4
baranyviki <vica.barany@gmail.com>
2019-10-27 22:04:27 +0100

    setting up study-buddy module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MatSidenavModule, MatListModule, MatToolbarModule, MatIcon, MatIconModule, MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { SidenavListComponent } from './components/sidenav-list/sidenav-list.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { StudyBuddyModule } from './study-buddy/study-buddy.module';
+import { RoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    SidenavListComponent,
+    MenuBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    RoutingModule,
+    StudyBuddyModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [LayoutComponent]
 })
 export class AppModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
+  apiUrl: 'http://localhost:38213/api',
   production: true
 };
